90d2327926933aa255688e402bc207db6f1b0843
Mihael Scofield <42939750+Mihael-Scofield@users.noreply.github.com>
2019-06-22 15:42:08 -0300

    includes.h
    Os includes de meus arquivos são jogados para cá

---
+++ includes.h
@@ -0,0 +1,10 @@
+/* Mihael Scofield de Azevedo - GRR 20182621 */
+/* Os includes de meus arquivos são jogados para cá. */
+
+/* Cabeçalhos */
+#include "structs.h"
+#include "manipulacao_avl.h"
+
+/* Bibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
